aab27c93453e69591cdf3f8f02fad9606222d47e
Aman Jangra <aman.12007267@lpu.in>
2023-05-02 16:06:36 +0530

    Navigation drawer Added

--- app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.expensetrackerapp
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.example.expensetrackerapp.Fragment.Adddata
 import com.example.expensetrackerapp.Fragment.SearchData
 import com.example.expensetrackerapp.databinding.ActivityMainBinding
-import com.example.expensetrackerapp.R
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +33,30 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+        drawerLayout = findViewById(R.id.my_drawer_layout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
 
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        // to make the Navigation drawer icon always appear on the action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+
     fun  replaceFragment(fragment : Fragment){
         val fragmentManager = supportFragmentManager
         val fragmentTransition= fragmentManager.beginTransaction()
